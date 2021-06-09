e1ccd6ef167de725aff896c9e92c9d45d4ca6a9d
Harsh Sharma <harshsharma7707@gmail.com>
2021-06-09 15:38:06 +0530

    added go back button in scanner screen

--- Cloudcode/warehouse/screens/Home/index.js
+++ Cloudcode/warehouse/screens/Home/index.js
@@ -6,23 +6,37 @@
  * @flow strict-local
  */
 import React from 'react';
-import type { Node } from 'react';
-import { SafeAreaView, View, Image, StatusBar, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, View, Image, StatusBar, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-
-const Home: () => Node = () => {
-
+const Home = ({navigation}) => {
     return (
         <SafeAreaView style={[styles.container, { backgroundColor: '#2099e7' }]}>
             <StatusBar barStyle="light-content" backgroundColor="#2099e7" />
             <View style={styles.scanner} >
-                <Image source={require('../../assets/qrscanner.png')} />
+                <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Scanner')}>
+                    <Image source={require('../../assets/qrscanner.png')} style={styles.scannerImage} />
+                </TouchableOpacity>
                 <View style={styles.textContainer} >
                     <View style={styles.textView} >
                         <Text style={styles.baseText}>Tracking Number</Text>
+                        <TextInput
+                            editable
+                            maxLength={40}
+                            value={'1234567890'}
+                            keyboardType="numeric"
+                            style={styles.input}
+                        />
                     </View>
                     <View style={styles.textView} >
                         <Text style={styles.baseText}>Reference Number</Text>
+                        <TextInput
+                            editable
+                            maxLength={40}
+                            value={'1234567890'}
+                            keyboardType="numeric"
+                            style={styles.input}
+                        />
                     </View>
                 </View>
             </View>
@@ -45,12 +59,16 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         paddingTop: '5%'
     },
+    scannerImage: {
+        height: hp('10%'),
+        width: wp('14%')
+    },
     info: {
         flex: 3,
         backgroundColor: 'white'
     },
     textContainer: {
-        display : 'flex',
+        display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
@@ -62,11 +80,23 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 5,
         borderColor: '#fff',
-        width : '40%',
+        width: '40%',
         margin: 6
     },
     baseText: {
-        color: '#fff'
+        color: '#fff',
+        fontSize: hp('2%')
+    },
+    input: {
+        marginTop: 6,
+        color: '#fff',
+        fontSize: hp('2%'),
+        borderBottomWidth: 1,
+        borderBottomColor: '#fff',
+        width: '44%'
+    }, 
+    btn: {
+
     }
 });
 
--- Cloudcode/warehouse/screens/Scanner/index.js
+++ Cloudcode/warehouse/screens/Scanner/index.js
@@ -5,45 +5,60 @@
  * @format
  * @flow strict-local
  */
- import React from 'react';
- import type {Node} from 'react';
+import React from 'react';
 
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
- import { RNCamera } from 'react-native-camera';
- 
- const Scanner: () => Node = () => {
- 
-   const onBarCodeRead =(e) => {
-     console.log('data', e.data)
-   }
- 
-   return (
-     <SafeAreaView  style={styles.container} >
-       <RNCamera captureAudio={false} style={styles.scanner} onBarCodeRead={onBarCodeRead} />
-     </SafeAreaView>
-   );
- };
- 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     flexDirection: 'column',
-     backgroundColor: 'black'
-   },
-   scanner: {
-     flex: 1,
-     justifyContent: 'flex-end',
-     alignItems: 'center'
-   }
- });
- 
- export default Scanner;
- 
+import {
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  Text,
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+
+const Scanner = ({ navigation }) => {
+
+  const onBarCodeRead = (e) => {
+    console.log('data', e.data)
+  }
+
+  return (
+    <SafeAreaView style={styles.container} >
+      <RNCamera captureAudio={false} style={styles.scanner} onBarCodeRead={onBarCodeRead} />
+      <Pressable
+        onPress={() => navigation.goBack()}
+        style={
+          styles.btn
+        }>
+          <Text style={styles.text}>
+          Go Back
+          </Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  scanner: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  btn : {
+    backgroundColor: '#2099e7',
+    padding: 10,
+  },
+  text :{ 
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: hp('2%')
+  }
+});
+
+export default Scanner;
